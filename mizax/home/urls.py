76570a0fcc9dcf17a99a08cc48e6f49cefec0b5d
mohamadebi <ebrahimimohamad9696@gmail.com>
2023-05-05 23:41:25 +0330

    first page

--- mizax/home/urls.py
+++ mizax/home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home),
     path('hello/',views.say_hello),
 
 ]
--- mizax/home/views.py
+++ mizax/home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def home(request):
+    return render(request,'home.html')
 def say_hello(request):
-    return HttpResponse('Hello user ...')
+    return render(request,'hello.html')
 # Create your views here.
