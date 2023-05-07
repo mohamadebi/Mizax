27fe8bd9ac50a546dd9667be8e6e07053777c296
mohamadebi <ebrahimimohamad9696@gmail.com>
2023-05-07 21:06:24 +0330

    create model

--- mizax/home/models.py
+++ mizax/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=25)
+    pupulation = models.IntegerField()
+    created = models.DateTimeField()
