b8165254c381274e855300fa364d85aca129c8d3
mohamadebi <ebrahimimohamad9696@gmail.com>
2023-05-07 21:25:46 +0330

    create base model in html

--- mizax/home/views.py
+++ mizax/home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Place
 
 def home(request):
-    return render(request,'home.html')
+    all = Place.objects.all()
+    return render(request,'home.html',{'all':all})
 def say_hello(request):
     return render(request,'hello.html')
 # Create your views here.
