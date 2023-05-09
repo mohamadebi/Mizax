7c5bfe07be0ef3b0633d8a9e196b13d4abd659be
mohamadebi <ebrahimimohamad9696@gmail.com>
2023-05-09 21:54:09 +0330

    add messages

--- mizax/home/urls.py
+++ mizax/home/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home,name = "home"),
     path('detail/<int:place_id>/',views.detail, name = "detail"),
-
+    path('delete/<int:place_id>/', views.delete, name="delete"),
 
 ]
--- mizax/home/views.py
+++ mizax/home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import Place
+from django.contrib import messages
 
 def home(request):
     all = Place.objects.all()
@@ -9,3 +10,9 @@ def home(request):
 def detail(requset,place_id):
      all = Place.objects.get(id=place_id)
      return  render(requset,'detail.html',{'place':all})
+
+
+def delete(requset, place_id):
+    Place.objects.get(id=place_id).delete()
+    messages.success(requset,'Record deleted successfully',extra_tags="success")
+    return redirect('home')
