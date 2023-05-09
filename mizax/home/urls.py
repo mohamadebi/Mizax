7779e997baf0143af6fff1b2ded8e82525037d7b
ebrahimi-m <ebrahimi-m@office.zharfpouyan.net>
2023-05-09 17:06:44 +0330

    niceproject

--- mizax/home/urls.py
+++ mizax/home/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('hello/',views.say_hello),
-    path('detail/<int:place_id>/',views.detail),
+    path('', views.home,name = "home"),
+    path('detail/<int:place_id>/',views.detail, name = "detail"),
 
 
 ]
--- mizax/home/views.py
+++ mizax/home/views.py
@@ -5,8 +5,6 @@ from .models import Place
 def home(request):
     all = Place.objects.all()
     return render(request,'home.html',{'all':all})
-def say_hello(request):
-    return render(request,'hello.html')
 # Create your views here.
 def detail(requset,place_id):
      all = Place.objects.get(id=place_id)
