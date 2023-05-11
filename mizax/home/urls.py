48a81f74379213e0bd495da03103fd75118a23a9
ebrahimi-m <ebrahimi-m@office.zharfpouyan.net>
2023-05-11 11:11:00 +0330

    niceproject1

--- mizax/home/urls.py
+++ mizax/home/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home,name = "home"),
     path('detail/<int:place_id>/',views.detail, name = "detail"),
-
-
+    path('delete/<int:place_id>/',views.delete),
 ]
