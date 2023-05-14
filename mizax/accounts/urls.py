c1966a70e0da8d78a7df44d58bc0666c241c12ad
mohamadebi <ebrahimimohamad9696@gmail.com>
2023-05-14 22:33:34 +0330

    create accounts

---
+++ mizax/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/',views.user_register, name='user_register')
+
+
+]
---
+++ mizax/accounts/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def user_register(request):
+    return HttpResponse('User registration page')
