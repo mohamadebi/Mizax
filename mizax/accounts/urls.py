040deb1edac7a2fb761feed6a142dd9488c9f8ee
mohamadebi <ebrahimimohamad9696@gmail.com>
2023-05-16 20:21:23 +0330

    login_page

--- mizax/accounts/urls.py
+++ mizax/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/',views.user_register, name='user_register')
-
+    path('register/',views.user_register, name='user_register'),
+    path('login',views.user_login,name='login')
 
 ]
--- mizax/accounts/views.py
+++ mizax/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from . forms import UserRegistrationForm
+from . forms import UserRegistrationForm , UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate,login
 
 def user_register(request):
     if request.method == 'POST' :
@@ -19,3 +20,20 @@ def user_register(request):
         form = UserRegistrationForm()
     return render(request,'register.html',{'form':form})
 
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,username = cd['username'],password=cd['password'])
+            if user is not None:
+                login(request,user)
+                messages.success(request,'logged in successfully','success')
+                return redirect('home')
+
+            else:
+                messages.error(request,'username or password is wrong','danger')
+
+    else:
+        form = UserLoginForm()
+    return render(request,'login.html',{'form':form})
