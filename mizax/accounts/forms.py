ab13fbd067e028aa6442f0d4bac62a4f8406ec79
mohamadebi <ebrahimimohamad9696@gmail.com>
2023-05-16 10:57:46 +0330

    adduserform

---
+++ mizax/accounts/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
